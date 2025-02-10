975af8f085ae268be55043b7c80899d3f420165b
uxu-hao <sakira_d@icloud.com>
2025-02-10 23:10:59 +0900

    feat: ドキュメントをもとにサンプルコードを作成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,51 +3,55 @@
 import * as THREE from "three";
 import { Canvas, ThreeElements, useFrame } from "@react-three/fiber";
 import { useRef, useState } from "react";
-
-function Box(props: ThreeElements["mesh"]) {
-  // This reference will give us direct access to the mesh
-  const meshRef = useRef<THREE.Mesh>(null!);
-  // Set up state for the hovered and active state
-  const [hovered, setHover] = useState(false);
-  const [active, setActive] = useState(false);
-  // Subscribe this component to the render-loop, rotate the mesh every frame
-  useFrame((state, delta) => (meshRef.current.rotation.x += delta));
-  // Return view, these are regular three.js elements expressed in JSX
-  return (
-    <mesh
-      {...props}
-      ref={meshRef}
-      scale={active ? 1.5 : 1}
-      onClick={() => setActive(!active)}
-      onPointerOver={() => setHover(true)}
-      onPointerOut={() => setHover(false)}
-    >
-      <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
-    </mesh>
-  );
-}
+import { div } from "three/tsl";
+import Hero from "./sections/Hero";
+// function Box(props: ThreeElements["mesh"]) {
+//   // This reference will give us direct access to the mesh
+//   const meshRef = useRef<THREE.Mesh>(null!);
+//   // Set up state for the hovered and active state
+//   const [hovered, setHover] = useState(false);
+//   const [active, setActive] = useState(false);
+//   // Subscribe this component to the render-loop, rotate the mesh every frame
+//   useFrame((state, delta) => (meshRef.current.rotation.x += delta));
+//   // Return view, these are regular three.js elements expressed in JSX
+//   return (
+//     <mesh
+//       {...props}
+//       ref={meshRef}
+//       scale={active ? 1.5 : 1}
+//       onClick={() => setActive(!active)}
+//       onPointerOver={() => setHover(true)}
+//       onPointerOut={() => setHover(false)}
+//     >
+//       <boxGeometry args={[1, 1, 1]} />
+//       <meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
+//     </mesh>
+//   );
+// }
 
 export default function Home() {
   return (
-    <div className="w-screen h-screen">
-      <Canvas>
-        <ambientLight intensity={Math.PI / 2} />
-        <spotLight
-          position={[10, 10, 10]}
-          angle={0.15}
-          penumbra={1}
-          decay={0}
-          intensity={Math.PI}
-        />
-        <pointLight
-          position={[-10, -10, -10]}
-          decay={0}
-          intensity={Math.PI}
-        />
-        <Box position={[-1.2, 0, 0]} />
-        <Box position={[1.2, 0, 0]} />
-      </Canvas>
+    // <div className="w-screen h-screen">
+    //   <Canvas>
+    //     <ambientLight intensity={Math.PI / 2} />
+    //     <spotLight
+    //       position={[10, 10, 10]}
+    //       angle={0.15}
+    //       penumbra={1}
+    //       decay={0}
+    //       intensity={Math.PI}
+    //     />
+    //     <pointLight
+    //       position={[-10, -10, -10]}
+    //       decay={0}
+    //       intensity={Math.PI}
+    //     />
+    //     <Box position={[-1.2, 0, 0]} />
+    //     <Box position={[1.2, 0, 0]} />
+    //   </Canvas>
+    // </div>
+    <div>
+      <Hero />
     </div>
   );
 }
