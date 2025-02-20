4c357bc209049409b948e154f272928f9c6d3f13
uxu-hao <sakira_d@icloud.com>
2025-02-20 21:49:15 +0900

    style: スタイル調整

--- src/app/sections/Hero.tsx
+++ src/app/sections/Hero.tsx
@@ -4,7 +4,7 @@ import HeroExperience from "../components/HeroExperience";
 export default function Hero() {
   return (
     <section className="min-h-screen w-full flex flex-col relative">
-      <div className="w-full h-100vh absolute inset-0">
+      <div className="w-full h-screen absolute inset-0">
         <HeroExperience />
       </div>
     </section>
