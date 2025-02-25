ad614f37db51df9e0416a9f623e8368e0cafbb74
uxu-hao <sakira_d@icloud.com>
2025-02-25 16:33:21 +0900

    chore: Worksセクションのサムネイル表示のためogp画像を入手

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'v-live-pi.vercel.app',  
+      },
+      {
+        protocol: 'https',
+        hostname: 'mbti-sage-eight.vercel.app',
+      },
+      {
+        protocol: 'https',
+        hostname: 'command-app.vercel.app',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
