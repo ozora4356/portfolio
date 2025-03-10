a50c6227b355f820fca61165cbb62e0697d9aa10
uxu-hao <sakira_d@icloud.com>
2025-03-11 06:44:47 +0900

    fix: serverレンダリング時にエラーが出ないよう修正

--- src/app/components/MouseTracker.tsx
+++ src/app/components/MouseTracker.tsx
@@ -3,6 +3,7 @@
 import styled from "@emotion/styled";
 import { useMousePosition } from "../hooks/useMousePosition";
 import { motion } from "framer-motion";
+import { useState, useEffect } from "react";
 
 const Cursor = styled(motion.div)`
   position: fixed;
@@ -17,11 +18,39 @@ const Cursor = styled(motion.div)`
 
 const MouseTracker = () => {
   const { x, y } = useMousePosition();
+  const [windowSize, setWindowSize] = useState({ width: 0, height: 0 });
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    // クライアントサイドでのみwindowオブジェクトを参照
+    setWindowSize({
+      width: window.innerWidth,
+      height: window.innerHeight,
+    });
+
+    const handleResize = () => {
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    };
+
+    window.addEventListener("resize", handleResize);
+    setIsMounted(true);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  // マウントされるまでは何も表示しない
+  if (!isMounted) return null;
+
   return (
     <Cursor
       animate={{
-        x: (x * window.innerWidth) / 2 + window.innerWidth / 2 - 10,
-        y: (-y * window.innerHeight) / 2 + window.innerHeight / 2 - 10,
+        x: (x * windowSize.width) / 2 + windowSize.width / 2 - 10,
+        y: (-y * windowSize.height) / 2 + windowSize.height / 2 - 10,
       }}
       transition={{
         type: "spring",
