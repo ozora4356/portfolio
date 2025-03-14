0cfe71aae210b08b33d27d0240c77d6f99a64dc6
uxu-hao <sakira_d@icloud.com>
2025-03-15 05:23:11 +0900

    refactor: モデルデータの調整

--- src/app/components/AboutExperience.tsx
+++ src/app/components/AboutExperience.tsx
@@ -1,8 +1,8 @@
 "use client";
 
-import { PerspectiveCamera, PresentationControls } from "@react-three/drei";
-import { Suspense, useMemo, useState, useEffect } from "react";
-import AboutModel from "../components/AboutModel";
+import { PerspectiveCamera } from "@react-three/drei";
+import { useMemo, useState, useEffect, Suspense } from "react";
+import AboutModel from "./AboutModel";
 import { Canvas } from "@react-three/fiber";
 import CanvasLoader from "./CanvasLoader";
 
@@ -19,7 +19,9 @@ const Scene = () => {
     setScreenWidth(window.innerWidth);
 
     const handleResize = () => {
-      setScreenWidth(window.innerWidth);
+      requestAnimationFrame(() => {
+        setScreenWidth(window.innerWidth);
+      });
     };
 
     window.addEventListener("resize", handleResize);
@@ -37,21 +39,21 @@ const Scene = () => {
     switch (viewportSize) {
       case "mobile":
         return {
-          scale: 1.2,
-          position: [-20, -60, 0] as [number, number, number],
+          scale: 1,
+          position: [0, -2.5, 0] as [number, number, number],
         };
 
       case "tablet":
         return {
-          scale: 0.9,
-          position: [-40, 0, 0] as [number, number, number],
+          scale: 1.2,
+          position: [0, -2, 0] as [number, number, number],
         };
 
       case "desktop":
       default:
         return {
-          scale: 1,
-          position: [-50, 0, 0] as [number, number, number],
+          scale: 1.2,
+          position: [0, -2, 0] as [number, number, number],
         };
     }
   }, [screenWidth]);
@@ -60,27 +62,17 @@ const Scene = () => {
     <>
       <PerspectiveCamera
         makeDefault
-        position={[0, 140, 800]}
+        position={[0, 0, 11]}
       />
-      <PresentationControls
-        global
-        rotation={[0.13, 0.1, 0]}
-        polar={[-0.2, 0.1]}
-        azimuth={[-1, 0.75]}
-        speed={1}
-        zoom={1}
-        snap
-      >
-        <AboutModel
-          scale={modelProps.scale}
-          position={modelProps.position}
-          rotation={[-Math.PI, -1.1, 0]}
-        />
-      </PresentationControls>
-      <ambientLight intensity={0.6} />
+      <AboutModel
+        scale={modelProps.scale}
+        position={modelProps.position}
+        rotation={[0.05, -1, 0]}
+      />
+      <ambientLight intensity={1} />
       <directionalLight
         position={[10, 10, 10]}
-        intensity={4}
+        intensity={3}
       />
     </>
   );
@@ -88,13 +80,7 @@ const Scene = () => {
 
 const AboutExperience = () => {
   return (
-    <Canvas
-      camera={{ position: [0, 140, 800], fov: 75 }}
-      style={{
-        width: "100%",
-        height: "100%",
-      }}
-    >
+    <Canvas>
       <Suspense fallback={<CanvasLoader />}>
         <Scene />
       </Suspense>
