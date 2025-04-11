24d7140cbe239f4be85c02459616f80de83488a8
uxu-hao <sakira_d@icloud.com>
2025-04-11 14:08:07 +0900

    chore: eslintを再定義

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,39 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends(
+    'next/core-web-vitals',
+    'plugin:@typescript-eslint/recommended',
+  ),
+  {
+    rules: {
+      // React
+      'react/jsx-boolean-value': 'error', // booleanプロップの記法を強制
+      'react/self-closing-comp': 'error', // 自己終了タグを強制
+      // TypeScript
+      '@typescript-eslint/no-unused-vars': [
+        // 未使用変数の警告
+        'error',
+        {
+          argsIgnorePattern: '^_', // _で始まる引数は無視
+          varsIgnorePattern: '^_', // _で始まる変数は無視
+        },
+      ],
+      '@typescript-eslint/consistent-type-imports': [
+        // 型インポートの一貫性
+        'error',
+        {
+          prefer: 'type-imports', // type importを強制
+        },
+      ],
+
+      // Common
+      'no-console': ['error', { allow: ['warn', 'error'] }], // console.logの警告
+      eqeqeq: 'error', // 厳密等価演算子の強制
+      'no-var': 'error', // varの使用を警告
+      'prefer-const': 'error', // letよりconstを推奨
+    },
+  },
 ];
 
 export default eslintConfig;
